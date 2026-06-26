32592e10789f520b9f304d920353212bdc7831be
goloshapovaeva <goloshapovaeva@gmail.com>
2026-06-26 16:44:54 +0700

    added loops page

--- playground.playground/Pages/loops.xcplaygroundpage/Contents.swift
+++ playground.playground/Pages/loops.xcplaygroundpage/Contents.swift
@@ -1,7 +1,43 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+for i in 0...100
+{
+    //print("операция применена \(i) раз")
+}
 
-//: [Next](@next)
+let names: [String] = ["Vova", "Max", "Anya", "Katya"]
+var femaleNames: [String] = []
+
+for name in names{
+    //print(name)
+    if name == "Anya" || name == "Katya"{
+        femaleNames.append(name)
+    }
+}
+
+//print(femaleNames)
+
+let story1: Bool = true
+let story2: Bool = true
+let story3: Bool = false
+let story4: Bool = false
+var stories: [Bool] = [story1, story2, story3, story4]
+
+for (ind, story) in stories.enumerated(){
+    if ind == 0{
+        continue
+    }
+    //print("index: \(ind) story is \(story)")
+    
+    if ind == 2{
+        break
+    }
+}
+
+var currentLevel: Int = 0
+let finalLevel: Int = 9
+while currentLevel <= finalLevel{
+    var notDone = finalLevel - currentLevel
+    print("Вы проши уровень №\(currentLevel), вам осталось \(notDone)")
+    currentLevel+=1
+}
